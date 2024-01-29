53e2830336f4a7f24c45e4eb46ad81f6cb4ffd0f
ummaar <155968282+eeziioo@users.noreply.github.com>
2024-01-29 19:16:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('1-sonni kiriting')
+let num2 = prompt('2-sonni kiriting')
+
+console.log(`
+sizni kiritgan 1-son bu (${num1}) endi sizning raqamingiz (${++num1}) o'zgardi
+sizni kiritgan 2-son bu (${num2}) endi sizning raqamingiz (${--num2}) o'zgartirildi
+`);
